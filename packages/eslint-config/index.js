21f92c4325e2dfc8d7d48f059cf01155a555ab4c
xuzhanyi <xuzhanyi@joyy.com>
2023-01-21 01:36:00 +0800

    feat: add eslint-config package

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    require.resolve('./shared/base'),
+    require.resolve('eslint-config-standard'),
+    'plugin:prettier/recommended'
+    // require.resolve('eslint-config-prettier/standard')
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  }
+}
---
+++ packages/eslint-config/shared/base.js
@@ -0,0 +1,12 @@
+/**
+ * 向eslint规则中添加自定义的全局变量供识别
+ * readonly/false——只读
+ * writable/true——可写
+ * off——禁用该全局变量
+ */
+module.exports = {
+  globals: {
+    // __DEV__: true,
+    // __PROD__: true
+  }
+}
